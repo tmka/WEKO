3010d9718890d0455d8291564e48b4b544c7b438
T.M <tmka.net@gmail.com>
2015-09-04 19:03:54 +0900

    add permalink_v2 file
    this file is used if-modified-header.

---
+++ permalink_v2.php
@@ -0,0 +1,76 @@
+<?php
+
+//You have to change the following variable when you use this code for other sites.
+$URL = "hoge.com";
+$BASE_URL = "https://" .$URL."/";
+$opensearch_URL = $BASE_URL . "index.php?action=repository_opensearch&format=atom&keyword=";
+$oaipmh_URL = $BASE_URL . "?action=repository_oaipmh&verb=GetRecord&medatadaPrefix=oai_dc&identifier=oai:".$URL.":";
+$download_URL = $BASE_URL. "index.php?action=repository_action_common_download&item_id=";
+
+function get_xml($keyword){
+	global $opensearch_URL;
+	$open_URL = $opensearch_URL;
+	$open_URL .= $keyword;
+	$XML = simplexml_load_file("$open_URL");
+	return $XML;
+}
+
+//return contents URL or thumbnail URL
+function contents_url($keyword){
+	$XML = get_xml($keyword);
+	foreach ($XML->entry as $item) {
+		$link = (string)$item->link->attributes()->href;
+	}
+	parse_str($link); //extract only $item_id
+	return $link;
+}
+
+function get_date($keyword){
+	$XML = get_xml($keyword);
+	foreach ($XML->entry as $item) {
+		$link = (string)$item->updated;
+	}
+	return $link;
+}
+
+global $BASE_URL,$download_URL;
+		$keyword = "20120101";
+		$item_id = contents_url($keyword);
+		if(is_null($item_id)){
+			header("HTTP/1.1 301 Moved Permanently");
+	     		header("Location: ".$BASE_URL);
+		}else{
+			$download_URL .= $item_id . "&item_no=1&attribute_id=1&file_no=1";
+			$now_date = date(DATE_RFC850,time());
+			$date = date(DATE_RFC850,strtotime(get_date($keyword)));
+
+			$If_Header = $_SERVER['HTTP_IF_MODIFIED_SINCE'];
+			header("Last-Modified: $date");
+			//echo $now_date;
+			//header('Last-Modified: Fri Jan 01 2010 00:00:00 GMT');
+			//echo $header;
+			//client send IF_MODIFIED HEADER
+			if($If_Header){
+				//echo "If_header =". strtotime($If_Header) . ".";
+				//echo "date =". strtotime($date) . ".";
+				if(strtotime($If_Header) == strtotime($date)){
+					//echo "no need update";
+					// not modified wo ireruto kousin sarenai?
+					header('HTTP/1.1 304 Not Modified');
+					exit();
+				}
+				else{
+					//echo "need update";
+					header("HTTP/1.1 301 Moved Permanently");
+	     				header("Location: ".$download_URL);
+				}
+			//client not send IF_MODIFIED HEADER			
+			}else{
+				//echo 'changed';
+				//var_dump($_SERVER);
+				header("HTTP/1.1 301 Moved Permanently");
+	     			header("Location: ".$download_URL);
+			}
+		}
+
+?>
